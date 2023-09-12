374810f763bb28a4ef1fe1d3506507f22a2f167f
Tijmen de Jong <eengamer2007@gmail.com>
2023-09-12 10:14:12 +0200

    it broke when i tried to detect %

--- printf.s
+++ printf.s
@@ -1,6 +1,6 @@
 .text
 	.global _start
-	string: .asciz "test test test or not \n test"
+	string: .asciz "test test test or not \n test %"
 
 _start:
 	mov $string, %rdi
@@ -16,21 +16,30 @@ _start:
 printf:
 	push %rbp				# prologue
 	mov %rsp, %rbp
-	mov %rdi, %r15
-	xor %rbx, %rbx
+
+	mov %rdi, %r15				# save string start at r15 because rdi will be used
+
+	xor %rbx, %rbx				# zero rbx to use as char counter
 	printloop:
 		
-		mov $1, %rax
+		cmpb $0x25, (%r15, %rbx)	# test for %
+		je format 
+		
+		mov $1, %rax			# print current character
 		mov $1, %rdi
 		lea (%r15, %rbx), %rsi
 		mov $1, %rdx
 		syscall
-
-		mov %rbx, %r10
-		inc %rbx
+		
+		mov %rbx, %r10			# check if the current char is 0 and if it is exit
+		inc %rbx			# else increment the char counter
 		cmpb $0, (%r15, %r10)
-		jnz printloop
+		je printloop
 	
+	format:
+		inc %rbx
+		jmp printloop
+
 	mov %rbp, %rsp
 	pop %rbp
 	ret
