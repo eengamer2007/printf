ce2b6928aebef54a9c433b26c36595de8d04790f
Tijmen de Jong <eengamer2007@gmail.com>
2023-09-12 14:04:44 +0200

    starting on decimal formatting

--- decimal_format.s
+++ decimal_format.s
@@ -2,11 +2,19 @@
 
 # inputs
 # rdi: int
-# rsi: output locationof string
+# rsi: output location of string
 int_to_str:
 	push %rbp
 	mov %rsp, %rbp
 	
+_loop:
+	dec rdi
+	jz end
+
+	
+
+	jmp loop
+_end:
 
 	mov %rbp, %rsp
 	pop %rbp
--- printf.s
+++ printf.s
@@ -5,43 +5,47 @@
 .include "decimal_format.s"
 
 _start:
-	mov $string, %rdi
+	movq $string, %rdi
 	call printf
 
-	mov $60, %rax
-	xor %rdi, %rdi
+	movq $60, %rax
+	xorq %rdi, %rdi
 	syscall
 
 # rdi: format string
 # rsi...: arugmets to be formated
 printf:
-	push %rbp				# prologue
-	mov %rsp, %rbp
+	pushq %rbp				# prologue
+	movq %rsp, %rbp
+	
+	pushq %r15
 
-	mov %rdi, %r15				# save string start at r15 because rdi will be used
+	movq %rdi, %r15				# save string start at r15 because rdi will be used
 
-	xor %rbx, %rbx				# zero rbx to use as char counter
+	xorq %rbx, %rbx				# zero rbx to use as char counter
 	printloop:
 		
-		cmpb $0x25, (%r15, %rbx)	# test for %
+		cmpb $37, (%r15, %rbx)		# test for %
 		je format 
 		
-		mov $1, %rax			# print current character
-		mov $1, %rdi
-		lea (%r15, %rbx), %rsi
-		mov $1, %rdx
+		movq $1, %rax			# print current character
+		movq $1, %rdi
+		leaq (%r15, %rbx), %rsi
+		movq $1, %rdx
 		syscall
 		
-		mov %rbx, %r10			# check if the current char is 0 and if it is exit
-		inc %rbx			# else increment the char counter
+		movq %rbx, %r10			# check if the current char is 0 and if it is exit
+		incq %rbx			# else increment the char counter
 		cmpb $0, (%r15, %r10)
 		je printloop
 	
 	format:
-		inc %rbx
+		incq %rbx
 		jmp printloop
 
+	popq %r15
+
 	mov %rbp, %rsp
-	pop %rbp
+	popq %rbp
 	ret
 
