184ab51a954e9dac0031ea94e96a0fb6c5004592
eengamer2007 <eengamer2007@gmail.com>
2023-09-21 17:25:02 +0200

    working now :) :wrench:

--- printf.s
+++ printf.s
@@ -1,12 +1,10 @@
 .text
 	.global _start
-	string: .asciz "test %% %d %u %s %g %u %u %u %u %u %u %u %u\n"
+	string: .asciz "test %% %d %u '%s' %g %u %u %u %u %u %u %u %u\n"
 	test_string: .asciz "test works"
-.include "print_number.s"
-.include "print_nul_string.s"
-
 
 _start:
+	# printf call
 	mov $0xFFFFFFFFFFFFFFF0, %rsi
 	mov %rsi, %rdx
 	mov $string, %rdi
@@ -26,12 +24,13 @@ _start:
 	syscall
 
 # rdi: format string
-# rsi...: arugmets to be formated
+# ...: arugmets to be formated
 not_my_printf:
-	popq %r11
-	movq %rbp, %r10
-	movq %rsp, %rbp
+	popq %r11				# save the return address in %r11
+	movq %rbp, %r10				# save %rbp to %r10
+	movq %rsp, %rbp				# finish the "modified" prologue
 
+	# push all the arguments onto the stack so that we can easily pop them off
 	pushq %r9
 	push %r8
 	push %rcx
@@ -49,6 +48,7 @@ not_my_printf:
 		cmpb $0, (%r8, %rcx)		# check if character is 0
 		jz _printf_end				# if character is 0 jump to end
 
+		# print the character, making sure we save all the vulnerable caller-saved registers
 		leaq (%r8, %rcx), %rdi
 		push %r11
 		push %r10
@@ -61,36 +61,41 @@ not_my_printf:
 		incq %rcx			# increment char counter and jump to loop start
 		jmp _printloop
 	_format:
-		incq %rcx
+		incq %rcx		# increase rcx to be the next character
 		
 		movb (%r8, %rcx), %r9b	# move next character into a reg for quick access
 		
+		# if we wanted, we could have optimized this further by making it branchless with a lookup table
 		cmpb $'%', %r9b		# check if %
 		je _percent_percent
 		
-		cmpb $'d', %r9b		# check if %
+		cmpb $'d', %r9b		# check if d
 		je _percent_d
 
-		cmpb $'u', %r9b		# check if %
+		cmpb $'u', %r9b		# check if u
 		je _percent_u
 
-		cmpb $'s', %r9b		# check if %
+		cmpb $'s', %r9b		# check if s
 		je _percent_s
 		
 		jmp _printf_else
 	_percent_percent:
+	# print a percent character
 	leaq (%r8, %rcx), %rdi
 	mov $print_char, %r9
 	jmp _call_printer
 	_percent_d:
+	# copy the address of the print_decimal into r9 to call it with the next value
 	pop %rdi
 	mov $print_decimal, %r9
 	jmp _call_printer
 	_percent_u:
+	# copy the address of the print_unsigned into r9 to call it with the next value
 	pop %rdi
 	mov $print_unsigned, %r9
 	jmp _call_printer
 	_percent_s:
+	# copy the address of the print_nul_string into r9 to call it with the next value
 	pop %rdi
 	mov $print_nul_string, %r9
 	jmp _call_printer
@@ -101,6 +106,8 @@ not_my_printf:
 		leaq -1(%r8, %rcx), %rsi	# loade percent and following character to be printed
 		movq $1, %rdi
 		movq $2, %rdx
+		
+		# save the vulnerable registers
 		push %r11
 		push %r10
 		push %rcx
@@ -108,11 +115,14 @@ not_my_printf:
 		pop %rcx
 		pop %r10
 		pop %r11
-
+		
+		# increase rcx to go to the next character
 		incq %rcx
 		jmp _printloop
 
 	_call_printer:
+		
+		# call the function we have moved in r9, save the vulnerable registers
 		push %r10
 		push %r11
 		push %rcx
@@ -121,15 +131,17 @@ not_my_printf:
 		pop %r11
 		pop %r10
 
+		# increase rcx to the next character
 		incq %rcx
 		jmp _printloop
 
 	_printf_end:	
-	popq %r8
+	# popq %r8
 
-	mov %rbp, %rsp
-	mov %r10, %rbp
-	jmp *%r11
+	# modified 'epilogue'
+	mov %rbp, %rsp # restore rsp
+	mov %r10, %rbp # restore rbp from r10
+	jmp *%r11      # return to the call location
 
 # rdi: address
 print_char:
@@ -145,3 +157,137 @@ print_char:
 	mov %rbp, %rsp
 	popq %rbp
 	ret
+
+
+
+# void print_unsigned(quad n)
+# prints the unsigned number n using syscalls
+print_unsigned:
+  # prologue
+  pushq %rbp
+  movq %rsp, %rbp
+
+  movq %rdi, %rax # rax is where the divisions happen
+  xor  %rdx, %rdx # make sure it is zero to avoid floating point segfaults
+  movq $10, %rcx  # move the divisor into rcx
+
+  _pun_loop:
+    div %rcx # divide rax by 10
+
+    # the result of the division is now stored in %rax
+    # the remainder of the division is now stored in %rdx
+
+    # add '0' to the digit to transform into the ascii value
+    addb  $'0', %dl
+
+    # push the single byte onto the stack (since it is stored backwards)
+    # we (ab)use this property in order to generate the string backwards and then print it the right way around
+    subq  $1, %rsp
+    movb  %dl, (%rsp)
+
+    xor %dl, %dl # zero out %dl to avoid floating point errors 
+
+    # check if we are done with all the divisions
+    cmp  $0, %rax
+
+    # if not continue looping
+    jnz _pun_loop
+
+  # syscall to write the number
+  movq $1, %rax # syscall 1 == sys_write
+  
+  movq $1, %rdi # fd 1 == stdout
+
+  movq %rsp, %rsi # starting address
+
+  movq %rbp, %rdx
+  subq %rsp, %rdx # mov the length (rbp - rsp) into rdx
+
+  syscall
+
+  # epilogue
+  mov %rbp, %rsp
+  popq %rbp
+
+  ret
+
+
+# saving the negative sign in memory to print it using syscalls
+minus: .byte '-'
+
+# void print_decimal(quad n)
+# prints the signed decimal number n using syscalls
+print_decimal:
+  # prologue
+  pushq %rbp
+  movq %rsp, %rbp
+ 
+  # check if n is negative
+  cmp $0, %rdi
+  jg  _pde_end
+
+  _pde_neg:
+    # print the negative sign
+    push %rdi
+    movq $1, %rax # syscall 1 == sys_write
+    movq $1, %rdi # fd 1 == stdout
+    movq $minus, %rsi # starting address
+    movq $1, %rdx # the length is 1
+    syscall
+    pop %rdi
+
+    # two's complement negate the parameter
+    neg %rdi
+
+  _pde_end:
+    # prints the number as an unsigned one
+    call print_unsigned
+
+  # epilogue
+  mov %rbp, %rsp
+  popq %rbp
+
+  ret
+
+
+# void print_nul_string(quad string_address)
+print_nul_string:
+  push  %rbp
+  movq  %rsp, %rbp
+	
+  push %r8
+  push %r9
+
+  movq  %rdi, %r8 # starting location of the string
+  movq  $0, %r9   # counter for the length
+
+  _pns_count_length_loop:
+    # move the next character into al
+    movb (%r8, %r9, 1), %al
+    # check if it is a nul character (0x00)
+    cmp  $0, %al
+    # if nul  stop counting
+    jz   _pns_end_length_loop
+
+    # increase the counter
+    inc  %r9
+    # continue looping
+    jmp  _pns_count_length_loop
+
+
+  _pns_end_length_loop:
+
+  movq $1, %rax # syscall 1 == sys_write
+  movq $1, %rdi # fd 1 == stdout
+  movq %r8, %rsi # starting address
+  movq %r9, %rdx # length
+  syscall
+ 
+
+  pop %r9
+  pop %r8
+
+  movq  %rbp, %rsp
+  popq  %rbp
+
+  ret
